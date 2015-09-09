21f873f89919b8ffb8aef82c0fb41d3cfb9c177e
Brett Ammeson <ammesonb@gmail.com>
2015-09-09 16:31:14 -0400

    Adds heuristic 3 function

--- heuristics.js
+++ heuristics.js
@@ -3,6 +3,17 @@
 // Heuristic 2, Dan
 
 // Heuristic 3, Brett
+/**
+ * Heuristic 3 returns the maximum of the horizontal and vertical distance
+ * @param square Object containing coordinates of current square
+ * @param goal Object with goal coordinates
+ * @returns Maximum of vertical/horizontal distance to goal
+ */
+function maxDistance(square, goal) {
+    horizDiff = Math.abs(goal.x - square.x);
+    vertDiff = Math.abs(goal.y - square.y);
+    return Math.max(horizDiff, vertDiff);
+}
 
 // Heuristic 4, Lukas
 
