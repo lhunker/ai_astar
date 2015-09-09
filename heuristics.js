084638d8567cb5df14aa8fd10d27b34499c01876
Dan Murray <dmmurray@wpi.edu>
2015-09-09 15:32:11 -0400

    Creates heuristic file

---
+++ heuristics.js
@@ -0,0 +1,11 @@
+// Heuristic 1, Lukas already wrote it by writing the A* framework
+
+// Heuristic 2, Dan
+
+// Heuristic 3, Brett
+
+// Heuristic 4, Lukas
+
+// Heuristic 5, Dan
+
+// Heuristic 6, Brett
