bb3239b04f103c6c3db96efba77f74ddd480cc9c
Brett Ammeson <ammesonb@gmail.com>
2015-09-11 12:11:23 -0400

    Adds count of deepest actions taken

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ function main() {
     var start = board.getStart();
     var firstNode = new Square(start, 0, 'Start', 'N', 0);
     frontiers.queue(firstNode);
+    var actions = 0;
 
     //sort in reverse order for pop
     while (!board.isGoal(frontiers.peek().getLocation())) {
@@ -45,6 +46,7 @@ function main() {
             process.exit(1);
         }
         var current = frontiers.dequeue();
+        if (current.getActions() > actions) actions = current.getActions();
         var newNodes = current.expand(board, heuristic[hNum]);
         expanded++;
         pushToQueue(newNodes);
@@ -55,6 +57,7 @@ function main() {
     //Print solution
     console.info('Path Score: ' + (100 - result.getActualCost()));
     console.info('Actions taken: ' + result.getActions());
+    console.info('Deepest action found: ' + actions);
     console.info('Nodes Expanded: ' + expanded);
     console.info('Path: ' + result.getPath());
 }
